749d8a6c5adefd87e398213c622b7b49bf868a51
RaulC01 <raul.cardenasib@uanl.edu.mx>
2024-09-16 20:49:08 -0600

    First commit

---
+++ hiddenfiles.psm1
@@ -0,0 +1,29 @@
+﻿function Get-HiddenFiles {
+    <#
+    .SYNOPSIS
+    Lista de archivos ocultos en una ruta específica.
+
+    .DESCRIPTION
+    Este cmdlet permite listar todos los archivos ocultos en una carpeta determinada, proporcionando la ruta completa de cada archivo.
+
+    .PARAMETER Path
+    La ruta de la carpeta donde se buscarán los archivos ocultos.
+
+    .EXAMPLE
+    Get-HiddenFiles -Path "C:\Users\Raul\Documents"
+    
+    .NOTES
+    Archivo de módulo: hiddenfiles.psm1
+    #>
+    
+    param (
+        [string]$path
+    )
+    if (Test-Path -Path $path) {
+        Get-ChildItem -Path $path -File -Hidden | ForEach-Object {
+            $_.FullName
+        }
+    } else {
+        Write-Host "La ruta especificada no existe." -ForegroundColor Red
+    }
+}
